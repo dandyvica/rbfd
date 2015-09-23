143f1e18ee2b4f5592944ed34f5e4f15943a1a14
Alain Viguier <dandyvica@gmail.com>
2015-09-23 18:54:51 +0200

    added fieldcontainer idiom

--- didioms/source/app.d
+++ didioms/source/app.d
@@ -4,52 +4,93 @@ import std.conv;
 import std.algorithm;
 import std.string;
 import std.process;
+import std.range;
 
-class F {
+class Field {
 
-	File _fh;
-	char[] line;
+	string name;
+	ulong length;
 
-	this(string fn) {
-		_fh = File(fn, "r");
+	this(string n) {
+		name = n;
 	}
 
-	bool empty() { return _fh.eof; }
-	ref char[] front() {
-		_fh.readln(line);
-		//line.length = line.length-1;
-	 	return line;
+	override string toString() { return name; }
+}
+
+
+
+
+class FieldContainer(T) {
+	alias TNAME = typeof(T.name);
+
+	T[] list;
+	T[][TNAME] map;
+
+	@property ulong size() { return list.length; }
+	@property ulong length() {
+		return sum(list.map!(e => e.length));
 	}
-	void popFront() { }
+
+	string getMembersData(string memberName) {
+		auto s = "return array(T.map!(e => e." ~ memberName ~ "));";
+		return (s);
+	}
+
+
+
+	void opOpAssign(string op)(T element) if (op == "~")
+	{
+		list ~= element;
+		map[element.name] ~= element;
+	}
+
+	void popFront() {
+		list = list[1..$];
+	}
+
+	/// slicing operators
+	T opIndex(size_t i) { return list[i]; }
+	T[] opIndex(TNAME name) { return map[name]; }
+	T[] opSlice(size_t i, size_t j) { return list[i..j]; }
+
+	/// remove elements
+	void remove(TNAME name) {
+		list = list.remove!(f => f.name == name);
+
+		// remove corresponding key
+		map.remove(name);
+	}
+
 
 
 }
+
+/*
+class FieldContainerRange(T) {
+	FieldContainer!T fields;
+
+	this(FieldContainer!T f) { fields = f; }
+
+	@property bool empty() const { return fields.length == 0; }
+	@property ref FieldContainer!T front() { return fields[0]; }
+	void popFront() { fields.popFront(); }
+}
+
+FieldContainerRange!T myRange(T)(T elem) {
+	return new FieldContainerRange!T(elem);
+}
+*/
+
+
 
 void main(string[] argv)
 {
-	char[] line;
-	auto fh = File(argv[1]);
-	while (!fh.eof) {
-		writef("before readln eof=%s, ", fh.eof);
-		fh.readln(line,std.ascii.newline);
-		writefln("line=<%s>, after readln eof=%s",chomp(line), fh.eof);
-	}
-	fh.close();
-}
-
-
-	auto f = new F(argv[1]);
-
-
-	foreach (l; f) {
-		writefln("2.line=<%s>",l);
+	auto c = new FieldContainer!Field();
+	foreach (i; 1..10) {
+		c ~= new Field("FIELD"~to!string(i));
 	}
 
-	foreach (l; f.filter!(s => s.length == 0)) {
-		write(l);
-	}
-
-
-
+	//writeln(c.myRange.take(2));
 
 }
