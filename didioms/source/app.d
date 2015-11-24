2468a04cba167a7438c955f3ce6cbfe2e9175148
Alain Viguier <dandyvica@gmail.com>
2015-11-24 18:20:18 +0100

    some didiom test

--- didioms/source/app.d
+++ didioms/source/app.d
@@ -7,6 +7,7 @@ import std.process;
 import std.range;
 import std.datetime;
 import std.concurrency;
+import std.regex;
 
 void spawnedFunction()
 {
@@ -29,6 +30,7 @@ void spawnedFunction()
 void main(string[] argv)
 {
 
+    /*
     File fh = File(argv[1], "r");
 
     if (argv[2] == "thread") 
@@ -52,5 +54,41 @@ void main(string[] argv)
         }
         f.close;
     }
+    */
 
+    File fh = File(argv[1], "r");
+//    auto r = regex(r"(FL.*\n){1}(LG.*\n(SG.*\n){1,4}){1,4}");
+
+    /*
+    foreach (ubyte[] buffer; fh.byChunk(4096))
+    {
+        /*
+        auto m = matchAll(cast(char[])buffer, r);
+        foreach (c; m) {
+            foreach (c1; c) {
+            writefln("<%s>", c1);
+            }
+        }
+        foreach (line; splitter(cast(char[])buffer, "\n"))
+        {
+            writeln(line);
+        }
+    }
+*/
+
+    auto line = new char[4096];
+    auto i = 0;
+ 
+    foreach (char ub; fh.byChunk(4096).joiner)
+    {
+        if (ub == '\n')
+        {
+            writefln("%.*s", i, line);
+            i = 0;
+        }
+        else 
+            line[i++] = ub;
+    }
+
+    fh.close;
 }
